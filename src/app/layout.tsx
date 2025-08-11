085a5db35029de36fd233ff9b9f80463cc882830
Rexofoam <jonathanfoong1997@gmail.com>
2025-08-11 08:58:21 +0800

    Add Google Tag Manager to RootLayout
    Integrated Google Tag Manager by injecting the GTM script in the <head> and adding a noscript fallback iframe in the body of RootLayout. This enables tracking and analytics for the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 import "./globals.css";
 import ToastProvider from "@/components/ToastProvider";
 
@@ -19,10 +20,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MZFCJTZS');
+            `,
+          }}
+        />
+      </head>
       <body className="antialiased min-h-screen flex flex-col">
-        <div className="flex-1">
-          {children}
-        </div>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MZFCJTZS"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        <div className="flex-1">{children}</div>
         <ToastProvider />
         <Footer />
         <Analytics />
